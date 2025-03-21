d90949ec84b83c78d4ca97cc51516b771163f6f5
Shubham Jaiswal <shujaisstudy@gmail.com>
2025-03-22 01:52:31 +0530

    Updated

---
+++ 13_functions2.js
@@ -0,0 +1,33 @@
+// we can declare function inside function.
+function one(){
+    const username = "Shubham"
+
+    function two(){
+        const iplTeam = "RCB"
+        console.log(username);        
+    }
+
+    two();    
+}
+
+one();
+
+// -------------------------------------------------------------------------------
+// Hoisting:
+
+addOne(5);// -> allowed
+
+function addOne(num){
+    return num + 1;
+}
+
+addOne(5);// -> allowed
+
+
+// addTwo(5);// -> not allowed (Error)
+
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(5);// -> allowed
